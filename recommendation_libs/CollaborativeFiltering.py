c7415352b21c2f128d1ec37e9afc49b31c6a7af0
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2017-01-11 09:18:52 +0100

    Series added, bug fixes in linear regression

--- recommendation_libs/CollaborativeFiltering.py
+++ recommendation_libs/CollaborativeFiltering.py
@@ -2,9 +2,32 @@ import numpy as np
 import math
 from LoadingData import Loader
 
+class RecSystem(object):
+
+    def __init__(self, trainSet):
+        self.predictor = CollaborativeFiltering(trainSet)
+
+    def getQueryFloatResult(self, queryTuple):
+        user = int(queryTuple[0])
+        item = int(queryTuple[1])
+        return self.processedDataRepresentation[item][user]
+
+    def getMultiQueryFloatResults(self, queryTuplesList):
+        multiQueryFloatResults = {}
+        for tempQueryTuple in queryTuplesList:
+            multiQueryFloatResults[tempQueryTuple] = self.getQueryFloatResult(tempQueryTuple)
+        return multiQueryFloatResults
+
+    def processInputArray(self):
+        self.processedDataRepresentation = self.predictor.getPredictions()
+        self.inputDataProcessed = True
+
 class CollaborativeFiltering(object):
 
     loader = Loader()
+
+    def __init__(self, trainSet):
+        self.trainSet = trainSet
 
     def getUsersIds(self, udata):
         users_ids = set()
@@ -28,10 +51,26 @@ class CollaborativeFiltering(object):
 
         for index in range(len(first)):
             if first[index] != 0 and second[index] != 0:
-                first_to_return.append(first[index])
-                second_to_return.append(second[index])
+                if math.fabs(first[index]) <= 5 and math.fabs(second[index]) <= 5:
+                    first_to_return.append(first[index])
+                    second_to_return.append(second[index])
 
         return first_to_return, second_to_return
+
+    def getMostSimilar(self, vector):
+        sum = 0
+
+        for (id, similarity) in vector:
+            sum += similarity
+
+        avg = float(sum)/len(vector)
+        toReturn = []
+
+        for (id, similarity) in vector:
+            if similarity >= avg:
+                toReturn.append((id, similarity))
+
+        return toReturn
 
     def cosineSimilarity(self, first, second):
 
@@ -62,22 +101,29 @@ class CollaborativeFiltering(object):
 
         return ratings_matrix
 
-    def itemsGenresMatrix(self):
-        content_array = self.loader.loadFromFile('../u.item')
-        genres_array = self.loader.loadFromFile('../u.genre')
-        array = [[0 for x in range(len(genres_array))] for y in range(len(content_array))]
+    def getPrediction(self, userId, itemId):
+        rated = []
+        item = self.trainSet[itemId]
 
-        for elem in content_array:
-            row = elem.split("|")
-            if len(row) > 1:
-                length = len(row)
-                for num in range(5, length):
-                    rownmb = int(row[0]) - 1
-                    clmnmb = num - 5
-                    array[int(row[0]) - 1][num - 5] = row[num]
+        for index in range(len(self.trainSet)):
+            if self.trainSet[index][userId] != 0:
+                rated.append((index, self.trainSet[index]))
 
-        content_array_to_return = np.matrix(array)
-        return content_array_to_return
+        counter = 0.0
+        denominator = 0.0
+
+        similarities = []
+
+        for item_rated in rated:
+            similarities.append((item_rated[0], self.cosineSimilarity(item, item_rated[1])))
+
+        similarities = self.getMostSimilar(similarities)
+
+        for index in range(len(similarities)):
+            counter += similarities[index][1] * rated[index][1][userId]
+            denominator += math.fabs(similarities[index][1])
+
+        return counter / denominator
 
     def getPredictionsForUser(self, matrix, user):
         not_rated = []
@@ -86,33 +132,38 @@ class CollaborativeFiltering(object):
 
         for index in range(len(matrix)):
             if matrix[index][user] == 0:
-                not_rated.append([index, matrix[index]])
+                not_rated.append((index, matrix[index]))
             else:
-                rated.append([index, matrix[index]])
+                rated.append((index, matrix[index]))
 
         counter = 0.0
         denominator = 0.0
+
+        #neighbours = int(math.ceil(float(len(rated))/1.5))
 
         for item in not_rated:
             similarities = []
 
             for item_rated in rated:
-                similarities.append(self.cosineSimilarity(item[1], item_rated[1]))
+                similarities.append((item_rated[0], self.cosineSimilarity(item[1], item_rated[1])))
+
+            similarities = self.getMostSimilar(similarities)
 
             for index in range(len(similarities)):
-                counter += similarities[index] * rated[index][1][user]
-                denominator += math.fabs(similarities[index])
+                counter += similarities[index][1] * rated[index][1][user]
+                denominator += math.fabs(similarities[index][1])
 
             predictions.append({'id': item[0], 'value': counter / denominator})
 
         return predictions
 
-    def getPredictions(self, matrix):
-        result = np.array(matrix)
+    def getPredictions(self):
+        self.trainSet = self.trainSet[1:, 1:]
+        result = np.array(self.trainSet)
 
-        for i in range(len(matrix[0])):
+        for i in range(len(self.trainSet[0])):
             userId = i
-            predictions = self.getPredictionsForUser(matrix, userId)
+            predictions = self.getPredictionsForUser(self.trainSet, userId)
 
             for j in range(len(predictions)):
                 movieId = predictions[j]['id']
